6a29888fe05ae2da7856e8ab3a739410116b8bf5
negbook <negbook@gmail.com>
2021-06-18 15:24:26 +0800

    Add files via upload

--- switchcase5.lua
+++ switchcase5.lua
@@ -1,44 +1,57 @@
-
 local case = {}
 local switch =
     setmetatable(
     {},
     {
-		__call = 	function(t, k)
-						
-						case = setmetatable({}, {__call = function(t,k) return t[k] end,
-						__index = 	
-						function(t,data) 
-							if data == k then 
-								return setmetatable({},{__call =	function(t, fn)
-										fn()
-									end
-									
-								})
-							else 
-								return (function() end)
-							end 
-							
-						end  })
-						return t[k]
-					end,
-        __index = 	function(t,data)
-						return setmetatable({},{__call =	function(t, ...)
-										
-									end
-									
-								})
-					end,
-		
+        __call = function(t, k)
+            case =
+                setmetatable(
+                {},
+                {
+                    __call = function(t, k)
+                        return t[k]
+                    end,
+                    __index = function(t, data)
+                        if data == k then
+                            return setmetatable(
+                                {},
+                                {
+                                    __call = function(t, fn)
+                                        fn()
+                                    end
+                                }
+                            )
+                        else
+                            return (function()
+                            end)
+                        end
+                    end
+                }
+            )
+            return t[k]
+        end,
+        __index = function(t, data)
+            return setmetatable(
+                {},
+                {
+                    __call = function(t, ...)
+                    end
+                }
+            )
+        end
     }
 )
 
-local a= 4
+local a = 4
 switch(a)(
-	case(1)(function()
-		print(99)
-	end),
-	case(4)(function()
-		print(98)
-	end)
+    case(1)(
+        function()
+            print(99)
+        end
+    ),
+    case(4)(
+        function()
+            print(98)
+        end
+    )
 )
